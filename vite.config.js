cc07ff58a64658b370cd99b3a71723e14d2a464a
Aryan <aryanchauhan4947@gmail.com>
2025-08-21 19:46:07 +0530

    react error solved

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
 })
